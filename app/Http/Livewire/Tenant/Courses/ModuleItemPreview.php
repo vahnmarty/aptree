b324e0891528159c61f5bea30610c7250fcbaf9d
Vahn <vahnmarty@gmail.com>
2023-02-17 07:01:26 +0800

    preview question

--- app/Http/Livewire/Tenant/Courses/ModuleItemPreview.php
+++ app/Http/Livewire/Tenant/Courses/ModuleItemPreview.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire\Tenant\Courses;
 
+use App\Models\Answer;
 use Livewire\Component;
 use App\Models\ModuleItem;
 
 class ModuleItemPreview extends Component
 {
     public $module;
+
+    public $selected_answer, $is_correct;
 
     public function render()
     {
@@ -17,5 +20,13 @@ class ModuleItemPreview extends Component
     public function mount($id)
     {
         $this->module = ModuleItem::find($id);
+    }
+
+    public function selectAnswer($answerId)
+    {
+        $answer = Answer::find($answerId);
+        
+        $this->selected_answer = $answerId;
+        $this->is_correct = $answer->is_correct;
     }
 }
