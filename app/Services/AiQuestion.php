748fcc2ffe02565aeb87e95e805710d7296ccdab
Vahn <vahnmarty@gmail.com>
2023-02-22 09:41:06 +0800

    ai question

--- app/Services/AiQuestion.php
+++ app/Services/AiQuestion.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Services;
-use Illuminate\Support\Facades\Http;
 use AWS;
+use GuzzleHttp\Client;
+use Aws\Lambda\LambdaClient;
+use GuzzleHttp\Psr7\Request;
+use Aws\Signature\SignatureV4;
+use Aws\Credentials\Credentials;
+use Illuminate\Support\Facades\Http;
 
 class AiQuestion {
 
@@ -27,30 +32,132 @@ class AiQuestion {
         $this->max = $max;
     }
 
+    public function createLambda()
+    {
+
+        $region = 'us-east-1';
+        $service = 'lambda';
+
+        // Define the AWS access key ID and secret access key to use
+        $accessKey = config('aws.credentials.key');
+        $secretKey = config('aws.credentials.secret');
+
+        // Define the HTTP method, URI, and query string parameters for your request
+        $method = 'GET';
+        $uri = 'https://dy6fp2ne5bayw4kgjbu2opuanq0lvxmg.lambda-url.us-east-1.on.aws/?path=mcq';
+        $query = [
+                'input_text' => 'Arthritis is the swelling and tenderness of one or more joints. The main symptoms of arthritis are joint pain and stiffness, which typically worsen with age. The most common types of arthritis are osteoarthritis and rheumatoid arthritis.Osteoarthritis causes cartilage — the hard, slippery tissue that covers the ends of bones where they form a joint — to break down. Rheumatoid arthritis is a disease in which the immune system attacks the joints, beginning with the lining of joints.', 
+
+                
+                'max_questions' => '2'
+            ];
+
+        // Define the HTTP headers for your request, including the "Host" and "X-Amz-Date" headers
+        $headers = [
+            'Host' => 'us-east-1.s3.amazonaws.com',
+            'X-Amz-Date' => gmdate('Ymd\THis\Z'),
+        ];
+
+        $client = new Client();
+
+        // Create a new SignatureV4 object to sign the request
+        $signer = new SignatureV4($service, $region);
+
+        // Sign the request
+        $credentials = new Credentials($accessKey, $secretKey);
+        $request = new Request('GET', $uri);
+        $request = $signer->signRequest($request, $credentials);
+
+        // Send the signed request and get the response
+        $response = $client->send($request);
+
+        return $response;
+    }
+
+    public function signature()
+    {
+        $credentials = new Credentials(config('aws.credentials.key'), config('aws.credentials.secret'));
+        $signer = new SignatureV4('questgen-api-QuestgenFunction-ILAfqAODMd1f', config('aws.region'));
+        $request = new Request('GET', 'https://dy6fp2ne5bayw4kgjbu2opuanq0lvxmg.lambda-url.us-east-1.on.aws/?path=mcq');
+        $signedRequest = $signer->signRequest($request, $credentials);
+
+        $client = new Client();
+        $response = $client->send($signedRequest);
+
+        dd('cliemt', $response);
+        $body = $response->getBody();
+        $json = json_decode($body, true);
+
+        return $json;
+    }
+
+    public function test()
+    {
+
+        $client = LambdaClient::factory([
+            'version' => 'latest',
+            'region'  => config('aws.region'),
+            'credentials' => [
+                'key'    => config('aws.credentials.key'),
+                'secret' => config('aws.credentials.secret'),
+            ]
+        ]);
+
+        $result = $client->invoke([
+            'FunctionName' => 'questgen-api-QuestgenFunction-ILAfqAODMd1f',
+            'Payload' => json_encode([
+                'path' => 'mcq',
+                'input_text' => "Arthritis is the swelling and tenderness of one or more joints. The main symptoms of arthritis are joint pain and stiffness, which typically worsen with age. The most common types of arthritis are osteoarthritis and rheumatoid arthritis.Osteoarthritis causes cartilage — the hard, slippery tissue that covers the ends of bones where they form a joint — to break down. Rheumatoid arthritis is a disease in which the immune system attacks the joints, beginning with the lining of joints.",
+                'max_questions' => 2
+            ])
+        ]);
+
+        $res = json_decode($result->get('Payload'), true);
+
+        dd($res);
+    }
+
+
     public function generate()
     {
+        $client = LambdaClient::factory([
+            'version' => 'latest',
+            'region'  => config('aws.region'),
+            'credentials' => [
+                'key'    => config('aws.credentials.key'),
+                'secret' => config('aws.credentials.secret'),
+            ]
+        ]);
+
+        $params = [
+            'path' => 'mcq',
+                'input_text' =>  "Arthritis is the swelling and tenderness of one or more joints. The main symptoms of arthritis are joint pain and stiffness, which typically worsen with age. The most common types of arthritis are osteoarthritis and rheumatoid arthritis.Osteoarthritis causes cartilage — the hard, slippery tissue that covers the ends of bones where they form a joint — to break down. Rheumatoid arthritis is a disease in which the immune system attacks the joints, beginning with the lining of joints.",
+                'max_questions' => 2
+        ];
+        $result = $client->invoke([
+            'FunctionName' => 'questgen-api-QuestgenFunction-ILAfqAODMd1f',
+            'Payload' => json_encode($params)
+        ]);
+
+        dd($result);
+
+        $data = $result['Payload']->getContents();
+
+        dd($data);
+
         // $lambda = AWS::createLambda()->invoke([
-        //     'FunctionName' => 'Flask API',
+        //     'FunctionName' => 'questgen-api-QuestgenFunction-ILAfqAODMd1f',
         //     'Payload' => json_encode([
-        //         'input_text' => "Arthritis is the swelling and tenderness of one or more joints. The main symptoms of arthritis are joint pain and stiffness, which typically worsen with age. The most common types of arthritis are osteoarthritis and rheumatoid arthritis.Osteoarthritis causes cartilage — the hard, slippery tissue that covers the ends of bones where they form a joint — to break down. Rheumatoid arthritis is a disease in which the immune system attacks the joints, beginning with the lining of joints.",
+        //         'path' => 'mcq',
+        //         'input_text' =>  "Arthritis is the swelling and tenderness of one or more joints. The main symptoms of arthritis are joint pain and stiffness, which typically worsen with age. The most common types of arthritis are osteoarthritis and rheumatoid arthritis.Osteoarthritis causes cartilage — the hard, slippery tissue that covers the ends of bones where they form a joint — to break down. Rheumatoid arthritis is a disease in which the immune system attacks the joints, beginning with the lining of joints.",
         //         'max_questions' => 2
         //     ])
         // ]);
 
-        // $output = json_decode($result['Payload'], true);
+        // $output = json_decode($lambda, true);
+
+        // dd($output);
 
         // return $output;
-
-        $response = Http::withHeaders([
-            'Authorization' => "AWS4-HMAC-SHA256 Credential=AKIAURB4ZGPGTARL2WNN/20230221/us-east-1/lambda/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=11f28938bfcfd6105ec97197db92889045496efbe9f52eb9fbc7fbe8448ec526",
-            'X-Amz-Content-Sha256' => 'beaead3198f7da1e70d03ab969765e0821b24fc913697e929e726aeaebf0eba3',
-            'X-Amz-Date' => '20230221T223238Z'
-        ])
-            ->get($this->url . '?path=mcq', [
-            'input_text' => $this->input,
-            'max_questions' => $this->max
-        ]);
-
-        return $response;
     }
 }
