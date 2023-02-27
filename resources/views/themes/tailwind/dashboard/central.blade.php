9d2b452c32a34eef2df7b833048067e825b5b404
vahnmarty <vahnmarty@gmail.com>
2023-02-27 09:45:17 +0800

    central fix

--- resources/views/themes/tailwind/dashboard/central.blade.php
+++ resources/views/themes/tailwind/dashboard/central.blade.php
@@ -11,7 +11,7 @@
 <div class="px-8 pt-6 pb-32 bg-gray-100">
 
 	<div class="grid grid-cols-4 gap-6">
-		<a href="" class="block p-4 bg-white border rounded-md shadow-md hover:bg-emerald-50">
+		<a href="{{ url('organization') }}" class="block p-4 bg-white border rounded-md shadow-md hover:bg-emerald-50">
 			<div>
 				<x-heroicon-s-globe class="w-10 h-10 text-gray-600"/>
 			</div>
@@ -19,7 +19,7 @@
 			<p class="mt-1 text-sm">Manage your Accounts.</p>
 		</a>
 
-		<a href="" class="block p-4 bg-white border rounded-md shadow-md hover:bg-emerald-50">
+		<a href="{{ url('billing') }}" class="block p-4 bg-white border rounded-md shadow-md hover:bg-emerald-50">
 			<div>
 				<x-heroicon-s-credit-card class="w-10 h-10 text-gray-600"/>
 			</div>
